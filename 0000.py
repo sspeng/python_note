6e16b391824bb8e274e9b8a83002aa31b28e70ad
jzwdsb <545373574@qq.com>
2017-06-12 22:47:28 +0800

    第一道题，在一张图片的右上角绘制数字，实现的一点灵活性都没有

---
+++ 0000.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+
+# PIL的文档说明 (http://pillow.readthedocs.io/en/latest/index.html)
+from PIL import Image, ImageDraw, ImageFont
+
+# 获取头像作为底板
+base = Image.open("avatar.png").convert('RGBA')
+
+# 创建一个新的透明图像，大小和底板大小相同，RGBA模式描述颜色的是一个四元组，最后一个参数是不透明度
+txt = Image.new('RGBA', base.size, (255, 255, 255, 0))
+
+# 获取一个字体，字体文件可以从当前工作目录或者从系统文件夹获取，第二个参数是大小
+fnt = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 100)
+
+# 获取一个绘制环境，
+d = ImageDraw.Draw(txt)
+
+
+# text()方法在当前的绘制上下文中绘制文字
+d.text((base.size[1] - 100, 0), '9', font=fnt, fill=(255, 0, 0, 255),)
+
+# d调用 Image.alpha_composite方法将第二个图像覆盖到第一个图像上
+out = Image.alpha_composite(base, txt)
+
+out.show()
