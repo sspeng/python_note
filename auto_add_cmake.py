765fe1f1851b03929d8636335fb67f8e98705b04
jzwdsb <545373574@qq.com>
2017-07-01 11:41:45 +0800

    生成当下.h和.cpp文件列表

--- auto_add_cmake.py
+++ auto_add_cmake.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 import os
 
-file_names = os.listdir()
+file_names = [ name for name in os.listdir() if name.endswith(('.h', '.cpp'))]
 file_names = ' '.join(file_names)
 
 name_list = open('name_list', 'w')
