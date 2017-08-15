b1360a5fa6ef522a44fc824b62a8e8c2280cae2f
jzwdsb <545373574@qq.com>
2017-08-15 17:36:24 +0800

    update

---
+++ tickets.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python3
+# coding=utf8
+
+"""命令行火车票查看器
+    Usage:
+        tickets     [-gdtkz]    <from>  <to>    <date>
+    Options:
+        -h, --help      显示帮助菜单
+        -g              高铁
+        -d              动车
+        -t              特快
+        -k              快速
+        -z              直达
+    Example:
+        tickets 北京  上海      2016-10-10
+        tickets -dg 成都  南京  2016-10-10
+"""
+
+from docopt import docopt
+
+
+def cli():
+    """command-line interface"""
+    arugments = docopt(__doc__)
+    print(arugments)
+
+
+if __name__ == '__main__':
+    cli()
+
