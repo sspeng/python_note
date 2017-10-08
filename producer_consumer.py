6281232e82b20500c65248014abaa7c25382890e
jzwdsb <545373574@qq.com>
2017-10-08 17:39:33 +0800

    fix consumer lost problem

--- producer_consumer.py
+++ producer_consumer.py
@@ -6,9 +6,13 @@ import queue
 import random
 import threading
 
-is_continue = True
+is_producing = True
+prod_lock = threading.Lock()
 
-MAXSIZE = 100
+is_moving = True
+move_lock = threading.Lock()
+
+MAXSIZE = 1000
 
 
 class Producer(threading.Thread):
@@ -17,13 +21,16 @@ class Producer(threading.Thread):
         self.prod_buffer = prod_buffer
 
     def run(self):
-        global is_continue
+        global is_producing
+        global prod_lock
         for i in range(MAXSIZE):
             print('%s : %s is producing %d to the producer buffer' % (time.ctime(), self.getName(), i))
             self.prod_buffer.put(random.randint(1, 1000))
             time.sleep(random.random())
         print('%s : %s finished!' % (time.ctime(), self.getName()))
-        is_continue = False
+        prod_lock.acquire()
+        is_producing = False
+        prod_lock.release()
 
 
 class Mover(threading.Thread):
@@ -34,8 +41,11 @@ class Mover(threading.Thread):
         self.con_buffer = con_buffer
 
     def run(self):
-        global is_continue
-        while is_continue:
+        global is_producing
+        global is_moving
+        global prod_lock
+        global move_lock
+        while not self.prod_buffer.empty() or is_producing:
             item = self.prod_buffer.get()
             time.sleep(random.random())
             print('%s : %s is moving %d to the Mover buffer' % (time.ctime(), self.getName(), item))
@@ -44,28 +54,44 @@ class Mover(threading.Thread):
             item = self.mov_buffer.get()
             print('%s : %s is moving %d to the Consumer buffer' % (time.ctime(), self.getName(), item))
             self.con_buffer.put(item)
+            prod_lock.acquire()
+            if not is_producing and self.prod_buffer.empty():
+                prod_lock.release()
+                break
+            prod_lock.release()
+        move_lock.acquire()
+        is_moving = False
+        move_lock.release()
 
 
 class Consumer(threading.Thread):
     def __init__(self, thread_name: str, con_buffer: queue.Queue):
         threading.Thread.__init__(self, name=thread_name)
         self.con_buffer = con_buffer
+        self.count = 0
         self.sum = 0
 
     def run(self):
-        global is_continue
-        while is_continue:
+        global is_moving
+        global move_lock
+        while not self.con_buffer.empty() or is_moving:
             item = self.con_buffer.get()
             print('%s : %s is consuming, %d in the queue is consumed!' % (time.ctime(), self.getName(), item))
             self.sum += item
-        print('%s : %s consumer finish, sum is %d' % (time.ctime(), self.getName(), self.sum))
+            self.count = self.count + 1
+            move_lock.acquire()
+            if not is_moving and self.con_buffer.empty():
+                move_lock.release()
+                break
+            move_lock.release()
+        print('%s : %s consumer finish, %d items is consumed, sum is %d'
+              % (time.ctime(), self.getName(), self.count, self.sum))
 
 
 def main():
     producer_buffer = queue.Queue(maxsize=MAXSIZE)
     mover_buffer = queue.Queue(maxsize=MAXSIZE)
     consumer_buffer = queue.Queue(maxsize=MAXSIZE)
-
     producer = Producer('producer', prod_buffer=producer_buffer)
     mover = Mover('Mover', prod_buffer=producer_buffer, mov_buffer=mover_buffer, con_buffer=consumer_buffer)
     consumer = Consumer('consumer', con_buffer=consumer_buffer)
