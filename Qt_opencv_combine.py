8a24b123509d32358504f4ef9dadcbcf42473c65
jzwdsb <545373574@qq.com>
2017-12-16 22:11:39 +0800

    the art of copy, paste and learning(-_-)

--- Qt_opencv_combine.py
+++ Qt_opencv_combine.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python3
 # -*- coding: utf8 -*-
 
+import sys
 from PyQt5.Qt import *
+import cv2
 
 class myLabel(QLabel):
     x0 = 0
@@ -28,7 +30,7 @@ class myLabel(QLabel):
         super().paintEvent(event)
         rect = QRect(self.x0, self.y0, abs(self.x1 - self.x0), abs(self.y1 - self.y0))
         painter = QPainter(self)
-        painter.setPen(Qt.red, 4, Qt.SolidLine)
+        painter.setPen(QPen(Qt.red, 4, Qt.SolidLine))
         painter.drawRect(rect)
 
         pqscreen = QGuiApplication.primaryScreen()
@@ -39,7 +41,28 @@ class myLabel(QLabel):
 class Example(QWidget):
     def __init__(self):
         super().__init__()
+        self.lb = myLabel(self)
         self.initUi()
 
     def initUi(self):
-        pass
+        self.resize(675, 300)
+        self.setWindowTitle("screenShot demo")
+        self.lb.setGeometry(140, 30, 511, 241)
+        img = cv2.imread('xxx.png')
+        height, width, bytePerComponent = img.shape
+        bytesPerline = 3 * width
+        cv2.cvtColor(img, cv2.COLOR_BGR2RGB, img)
+        Qimg = QImage(img.data, width, height, bytesPerline, QImage.Format_RGB888)
+        pixmap = QPixmap.fromImage(Qimg)
+
+        self.lb.setPixmap(pixmap)
+        self.lb.setCursor(Qt.CrossCursor)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Example()
+    window.show()
+
+    sys.exit(app.exec())
